9dde0ca0dd54e8244d7830834ff05de001facfe5
Maksim Golitsyn <maxim@infofree.ru>
2018-04-01 17:48:42 +0500

    Animation Timings

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,29 +3,32 @@ import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
-const modal = (props) => {
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
 
-  return (
-    <Transition
-      in={props.show}
-      timeout={300}
-      mountOnEnter
-      unmountOnExit
-    >
-      {state => {
+const modal = props => (
+  <Transition
+    in={props.show}
+    timeout={animationTiming}
+    mountOnEnter
+    unmountOnExit
+  >
+    {(state) => {
         const cssClasses = [
           'Modal',
           state === 'entering' ? 'ModalOpen'
-            : state === 'exiting' ? 'ModalClosed' : null
+            : state === 'exiting' ? 'ModalClosed' : null,
         ];
         return (
-        <div className={cssClasses.join(' ')}>
-          <h1>A Modal</h1>
-          <button className="Button" onClick={props.closed}>Dismiss</button>
-        </div>
-      )}}
-    </Transition>
-  );
-};
+          <div className={cssClasses.join(' ')}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>Dismiss</button>
+          </div>
+      );
+}}
+  </Transition>
+);
 
 export default modal;
